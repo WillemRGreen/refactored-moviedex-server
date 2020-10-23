bbc344611cea9073c86470db0a9bacd5121aae91
Willem Green <willberforce15@gmail.com>
2020-10-23 14:02:25 -0500

    total refactor

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const logger = require('./logger.js')
+const movies = require('./store.js')
 
 const app = express()
 
@@ -14,9 +16,39 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
+})
+
+app.get('/movie', (req, res) => {
+  let response = movies;
+
+  if(req.query.genre){
+    response = response.filter(movie => 
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+  }
+
+  if(req.query.country){
+    response = response.filter(movie => 
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
+  }
+
+  if(req.query.avg_vote){
+    response = response.filter(movie =>
+      movie.avg_vote >= req.query.avg_vote)
+  }
+  res.json(response)
 })
 
 app.use(function errorHandler(error, req, res, next) {
@@ -24,7 +56,6 @@ app.use(function errorHandler(error, req, res, next) {
        if (NODE_ENV === 'production') {
          response = { error: { message: 'server error' } }
        } else {
-         console.error(error)
          response = { message: error.message, error }
        }
        res.status(500).json(response)
